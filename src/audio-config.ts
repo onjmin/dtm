d15e945e58031d76ad966174ba1fbfaf65dbc90f
おんJ民 <J onjmin@users.noreply.github.com>
2026-05-03 01:33:51 +0900

    fix

--- src/audio-config.ts
+++ src/audio-config.ts
@@ -51,6 +51,7 @@ export function setupRecorder(
 			isRecording = false;
 		},
 		getRecordedData: () => recordedData,
+		isRecording: () => isRecording,
 		clearRecordedData: () => {
 			recordedData = [[], []];
 		},
