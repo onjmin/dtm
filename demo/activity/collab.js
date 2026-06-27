9da8927be5918c4ff86dd4d76924ff680b330f61
onjmin <onjmin@users.noreply.github.com>
2026-06-27 05:12:23 +0000

    deploy: d685fae0c6ced9df60a3055b7c9a8026762b0c86

--- demo/activity/collab.js
+++ demo/activity/collab.js
@@ -455,7 +455,7 @@ const initDAW = async (spectator = false) => {
     const isLocal = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
     const DTM = await import(isLocal
         ? 'http://localhost:40298/dist/index.mjs'
-        : '/.proxy/dtm/demo/index.mjs?v=b86e5ca1');
+        : '/.proxy/dtm/demo/index.mjs?v=d685fae0');
 
     const { createDtmStudio, TRACKS_ADVANCED } = DTM;
 
